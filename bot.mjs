e137145c83eb2f90fbf7fbc8ac0be2ab16c94a1b
Toddyn <jmiguel.franchini@gmail.com>
2024-08-24 10:18:43 -0300

    modified

--- bot.mjs
+++ bot.mjs
@@ -1,1 +1,23 @@
-console.log('testing github actions');
+import { Client, Partials, Collection, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds
+    , GatewayIntentBits.GuildMessages
+    , GatewayIntentBits.MessageContent
+    , GatewayIntentBits.DirectMessages
+    , GatewayIntentBits.DirectMessageTyping
+    , GatewayIntentBits.GuildMessageReactions
+    , GatewayIntentBits.GuildVoiceStates
+    , GatewayIntentBits.GuildMembers
+    , GatewayIntentBits.GuildPresences
+    , GatewayIntentBits.GuildMessageTyping
+
+], 
+partials: [ Partials.Channel]
+});
+
+export default client
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+})
+
+client.login(process.env.TOKEN)
