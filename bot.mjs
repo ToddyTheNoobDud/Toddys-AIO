4601b83615f6b61ef55bb51d31e5234d2e59a807
Toddyn <jmiguel.franchini@gmail.com>
2024-08-24 10:27:18 -0300

    updated

--- bot.mjs
+++ bot.mjs
@@ -1,23 +1,28 @@
-import { Client, Partials, Collection, GatewayIntentBits } from 'discord.js';
+import dotenv from "dotenv";
+dotenv.config();
+import { Client, Partials, Collection, GatewayIntentBits } from "discord.js";
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds
-    , GatewayIntentBits.GuildMessages
-    , GatewayIntentBits.MessageContent
-    , GatewayIntentBits.DirectMessages
-    , GatewayIntentBits.DirectMessageTyping
-    , GatewayIntentBits.GuildMessageReactions
-    , GatewayIntentBits.GuildVoiceStates
-    , GatewayIntentBits.GuildMembers
-    , GatewayIntentBits.GuildPresences
-    , GatewayIntentBits.GuildMessageTyping
-
-], 
-partials: [ Partials.Channel]
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageTyping,
+  ],
+  partials: [Partials.Channel],
 });
 
-export default client
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-})
+export default client;
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
+
+
