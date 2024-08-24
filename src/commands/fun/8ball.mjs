f170487c86a8843d3f419b399dcbb054162b51d2
Toddyn <jmiguel.franchini@gmail.com>
2024-08-24 10:46:53 -0300

    updated

--- src/commands/fun/8ball.mjs
+++ src/commands/fun/8ball.mjs
@@ -1,3 +1,5 @@
+import { EmbedBuilder } from "discord.js";
+
 export const Command = {
     name: "8ball",
     description: "Ask the 8ball a question!",
@@ -14,13 +16,37 @@ export const Command = {
         const question = interaction.options.getString("question");
 
         const responses = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
             "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
             "Ask again later.",
             "Better not tell you now.",
             "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
         ];
 
         const response = responses[Math.floor(Math.random() * responses.length)];
-        await interaction.reply(`Question: ${question}\nAnswer: ${response}`);
+        const embed = new EmbedBuilder()
+            .setColor(0,0,0)
+            .setAuthor({ name: "8ball", iconURL: client.user.displayAvatarURL() })
+            .setDescription(`Q: **${question}**\n\nR: ${response}`)
+            .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+        await interaction.reply({ embeds: [embed], ephemeral: false });
     }
 };
+
+
--- src/commands/info/ping.mjs
+++ src/commands/info/ping.mjs
@@ -1,7 +1,24 @@
+import { EmbedBuilder } from "discord.js";
+
 export const Command = {
     name: "ping",
-    description: "Ping!",
+    description: "Get the ping of the bot",
+    options: [],
     run: async (client, interaction) => {
-        await interaction.reply("Pong!");
-    },
+        const startTime = Date.now();
+        const initialMessage = await interaction.reply({ content: "Pinging...", fetchReply: true });
+
+        const embed = new EmbedBuilder()
+            .setColor(0,0,0)
+            .setTitle("Pong!")
+            .addFields(
+                { name: "API Ping", value: `${client.ws.ping}ms`, inline: true },
+                { name: "Latency", value: `${Date.now() - startTime}ms`, inline: true },
+                { name: "Shard ID", value: `${client.shard?.ids[0] ?? "None"}`, inline: true },
+                { name: "Shard Count", value: `${client.shard?.count ?? "None"}`, inline: true },
+            )
+            .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() });
+
+        await initialMessage.edit({ content: " ", embeds: [embed] });
+    }
 }
